d2dc36890fe1a37507874eaf88209bde55457a5c
JIMMY LEBLANC <jimmy.leblanc@etu.univ-nantes.fr>
2016-11-28 23:26:05 +0100

    commit pour régler pb de hierarchie

--- hashcode/hashcode/src/location.cpp
+++ hashcode/hashcode/src/location.cpp
@@ -1,5 +1,26 @@
 #include "location.hpp"
+#include "..\include\location.hpp"
 
 location::location(double latitude, double longitude) : m_latitude(latitude), m_longitude(longitude){
 
 }
+
+location::~location()
+{
+}
+
+location::location(const location & location)
+{
+	m_latitude = location.m_latitude;
+	m_longitude = location.m_longitude
+}
+
+location & location::operator=(const location& location)
+{
+	m_latitude = location.m_latitude;
+	m_longitude = location.m_longitude;
+	
+	return *this;
+}
+
+
