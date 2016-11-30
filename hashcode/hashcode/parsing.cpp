a6a48eb570e9cf631534e82b8a53f8e9925fda9d
jimmy <jimmy.leblanc@etu.univ-nantes.fr>
2016-11-30 19:10:19 +0100

    ajout de parsing.cpp

---
+++ hashcode/hashcode/parsing.cpp
@@ -0,0 +1,16 @@
+#include "satellite.hpp"
+#include "collection.hpp"
+#include <iostream>
+#include <fstream>
+
+
+void parse(const char* input_file) {
+	std::ifstream input;
+	input.open(input_file);
+
+
+
+
+
+	input.close();
+}
