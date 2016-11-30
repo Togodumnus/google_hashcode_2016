5d5a9f58247b07af3b868d7c746d6cdb62256cb7
jimmy <jimmy.leblanc@etu.univ-nantes.fr>
2016-11-30 15:35:03 +0100

    suppression classes CameraOrientation inutile

--- hashcode/hashcode/satellite.cpp
+++ hashcode/hashcode/satellite.cpp
@@ -1,7 +1,7 @@
 #include "satellite.hpp"
 
 
-satellite::satellite(simulation* simulation, double latitude, double longitude, float velocity, float orientation_max_change, float orientation_max_value, CameraOrientation cameraOrientation) : m_simulation(simulation), m_location(latitude, longitude), m_velocity(velocity), m_orientation_max_change(orientation_max_change), m_orientation_max_value(orientation_max_value){
+satellite::satellite(simulation* simulation, double latitude, double longitude, float velocity, float orientation_max_change, float orientation_max_value) : m_simulation(simulation), m_location(latitude, longitude), m_velocity(velocity), m_orientation_max_change(orientation_max_change), m_orientation_max_value(orientation_max_value){
 	
 }
 
@@ -9,7 +9,7 @@ satellite::~satellite(){
 
 }
 
-satellite::satellite(const satellite& satellite) : m_location(satellite.m_location), cameraOrientation(cameraOrientation){
+satellite::satellite(const satellite& satellite) : m_location(satellite.m_location){
 	m_simulation = satellite.m_simulation;
 	m_velocity = satellite.m_velocity;
 	m_orientation_max_change = satellite.m_orientation_max_change;
