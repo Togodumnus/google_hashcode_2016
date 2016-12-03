416c66e6c1f0bbeb798c8f5af5de358363cf05e3
Guillaume Clochard <guillaume.clochard@etu.univ-nantes.fr>
2016-12-03 21:23:01 +0100

    Fix indent

--- hashcode/hashcode/Simulation.cpp
+++ hashcode/hashcode/Simulation.cpp
@@ -7,24 +7,24 @@ Simulation::Simulation(const char* input_file) {
 
 Simulation::~Simulation()
 {
-    for (auto it = m_satellites.begin(); it != m_satellites.end(); it++) {
-        delete (*it);
-    }
-    m_satellites.clear();
+	for (auto it = m_satellites.begin(); it != m_satellites.end(); it++) {
+		delete (*it);
+	}
+	m_satellites.clear();
 
-    for (auto it = m_collections.begin(); it != m_collections.end(); it++) {
-        delete (*it);
-    }
-    m_collections.clear();
+	for (auto it = m_collections.begin(); it != m_collections.end(); it++) {
+		delete (*it);
+	}
+	m_collections.clear();
 
-    // TODO delete all photgraphs taken
+	// TODO delete all photgraphs taken
 }
 
 Simulation& Simulation::operator=(const Simulation& simulation)
 {
 	m_duration = simulation.m_duration;
 	m_number_of_collections = simulation.m_number_of_collections; // copy
-	m_number_of_satellites  = simulation.m_number_of_satellites;  // copy
+	m_number_of_satellites	= simulation.m_number_of_satellites;  // copy
 
 	return *this;
 }
@@ -33,5 +33,5 @@ Simulation::Simulation(const Simulation& simulation)
 {
 	m_duration = simulation.m_duration;
 	m_number_of_collections = simulation.m_number_of_collections; // copy
-	m_number_of_satellites  = simulation.m_number_of_satellites;  // copy
+	m_number_of_satellites	= simulation.m_number_of_satellites;  // copy
 }
