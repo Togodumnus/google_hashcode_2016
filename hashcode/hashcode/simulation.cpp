6eb96d6ad3d3d94a4b88d79b3dc10d88bf031e2e
Guillaume Clochard <guillaume.clochard@etu.univ-nantes.fr>
2016-12-03 05:46:38 +0100

    Clean simulation class

--- hashcode/hashcode/simulation.cpp
+++ hashcode/hashcode/simulation.cpp
@@ -1,24 +1,28 @@
 #include "simulation.hpp"
 #include <fstream>
 
-simulation::simulation(){
-}
+simulation::simulation() {}
 
 simulation::~simulation()
 {
-	m_duration = 0;
-	m_number_of_satellites = 0;
-	m_number_of_collections = 0;
-	m_photographs_taken = *(new std::vector<photograph*>);
-	m_satellites = *(new std::vector<satellite*>);
-	// TODO delete all satellites in m_satellites
+    for (auto it = m_satellites.begin(); it != m_satellites.end(); it++) {
+        delete (*it);
+    }
+    m_satellites.clear();
+
+    for (auto it = m_collections.begin(); it != m_collections.end(); it++) {
+        delete (*it);
+    }
+    m_collections.clear();
+
+    // TODO delete all photgraphs taken
 }
 
 simulation & simulation::operator=(const simulation & simulation)
 {
 	m_duration = simulation.m_duration;
-	m_number_of_collections = simulation.m_number_of_collections;
-	m_number_of_satellites = simulation.m_number_of_satellites;
+	m_number_of_collections = simulation.m_number_of_collections; // copy
+	m_number_of_satellites  = simulation.m_number_of_satellites;  // copy
 
 	return *this;
 }
@@ -26,35 +30,6 @@ simulation & simulation::operator=(const simulation & simulation)
 simulation::simulation(const simulation & simulation)
 {
 	m_duration = simulation.m_duration;
-	m_number_of_collections = simulation.m_number_of_collections;
-	m_number_of_satellites = simulation.m_number_of_satellites;
-}
-
-void simulation::setDuration(unsigned int duration)
-{
-	this->m_duration = duration;
-}
-
-void simulation::setSatellites(unsigned int satellitesNumber)
-{
-	this->m_number_of_satellites = satellitesNumber;
-}
-
-std::vector<satellite*> simulation::getSatellites()
-{
-	return this->m_satellites;
-}
-
-unsigned int simulation::getDuration()
-{
-	return this->m_duration;
-}
-
-void simulation::took_photograph(satellite* satellite, photograph* photograph){
-	m_photographs_taken.push_back(photograph);
-
-}
-
-void simulation::addSatellite(satellite* satellite) {
-	m_satellites.push_back(satellite);
+	m_number_of_collections = simulation.m_number_of_collections; // copy
+	m_number_of_satellites  = simulation.m_number_of_satellites;  // copy
 }
--- hashcode/hashcode/simulation.hpp
+++ hashcode/hashcode/simulation.hpp
@@ -10,21 +10,36 @@ class satellite;
 class simulation{
 
 	private:
-		unsigned int m_duration;
-		unsigned int m_number_of_satellites;
-		unsigned int m_number_of_collections;
+		unsigned long int m_duration;
+		unsigned int	  m_number_of_satellites;
+		unsigned int	  m_number_of_collections;
+
+		// TODO missing info (class "shoot" instead ? or attributes on class
+		// photograph ?)
 		std::vector<photograph*> m_photographs_taken;
-		std::vector<satellite*> m_satellites;
+
+		std::vector<satellite*>  m_satellites;
+		std::vector<collection*> m_collections;
 
 	public:
 		simulation();
 		~simulation();
 		simulation& operator=(const simulation& simulation);
 		simulation(const simulation& simulation);
-		void setDuration(unsigned int duration);
-		void setSatellites(unsigned int satellitesNumber);
-		std::vector<satellite*> getSatellites();
-		unsigned int getDuration();
-		void took_photograph(satellite*, photograph*);
-		void addSatellite(satellite *);
+
+		inline void setDuration(unsigned int duration) {
+			m_duration = duration;
+		}
+
+		inline void setSatellitesNumber(unsigned int satellitesNumber) {
+			m_number_of_satellites = satellitesNumber;
+		}
+
+		inline unsigned int getDuration() {
+			return this->m_duration;
+		}
+
+		inline void addSatellite(satellite* satellite) {
+			m_satellites.push_back(satellite);
+		}
 };
