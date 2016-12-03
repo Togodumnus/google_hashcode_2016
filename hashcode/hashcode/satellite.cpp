e79658a4af1b360c3279da5954d476d6b750f593
Guillaume Clochard <guillaume.clochard@etu.univ-nantes.fr>
2016-12-03 05:35:14 +0100

    Clean satellite class

--- hashcode/hashcode/satellite.cpp
+++ hashcode/hashcode/satellite.cpp
@@ -1,54 +1,61 @@
+#include <ostream>
+#include <array>
 #include "satellite.hpp"
 
+satellite::satellite(
+		simulation* simulation,
+		long int latitude,
+		long int longitude,
+		int velocity,
+		int orientation_max_change,
+		int orientation_max_value) :
+		location(latitude, longitude),
+		m_simulation(simulation),
+		m_velocity(velocity),
+		m_orientation_max_change(orientation_max_change),
+		m_orientation_max_value(orientation_max_value) { }
 
-satellite::satellite()
-{
+satellite::satellite(
+		simulation* simulation,
+		SatelliteLine line) :
+		m_simulation(simulation) {
+	location(std::stol(line[0]), std::stol(line[1]));
+	m_velocity               = std::stoi(line[2]);
+	m_orientation_max_change = std::stoi(line[3]);
+	m_orientation_max_value  = std::stoi(line[4]);
 }
 
-satellite::satellite(simulation* simulation, double latitude, double longitude, float velocity, float orientation_max_change, float orientation_max_value) : m_simulation(simulation), m_latitude(latitude), m_longitude(longitude), m_velocity(velocity), m_orientation_max_change(orientation_max_change), m_orientation_max_value(orientation_max_value){
-
-}
-
-satellite::~satellite(){
-
-}
+satellite::~satellite() { }
 
 satellite::satellite(const satellite& satellite) {
-	m_latitude = satellite.m_latitude;
-	m_longitude = satellite.m_longitude;
+	m_latitude	 = satellite.m_latitude;
+	m_longitude  = satellite.m_longitude;
 	m_simulation = satellite.m_simulation;
-	m_velocity = satellite.m_velocity;
+	m_velocity	 = satellite.m_velocity;
 	m_orientation_max_change = satellite.m_orientation_max_change;
 	m_orientation_max_value = satellite.m_orientation_max_value;
 }
 
-
 satellite & satellite::operator=(const satellite & satellite)
 {
+	m_latitude	 = satellite.m_latitude;
+	m_longitude  = satellite.m_longitude;
 	m_simulation = satellite.m_simulation;
-	m_latitude = satellite.m_latitude;
-	m_longitude = satellite.m_longitude;
-	m_velocity = satellite.m_velocity;
+	m_velocity	 = satellite.m_velocity;
 	m_orientation_max_change = satellite.m_orientation_max_change;
 	m_orientation_max_value = satellite.m_orientation_max_value;
 
 	return *this;
 }
 
-float satellite::getVelocity()
-{
-	return m_velocity;
-}
-
-
-// TODO
-/*bool satellite::canTake(photograph* photograph) {
-}*/
-
-bool satellite::take(photograph* photograph){
-	return false;
-}
-
-void satellite::moveCamera()
-{
+std::ostream& operator<<(std::ostream& o, const satellite& s) {
+	return o << "Satellite("
+		<< "lat[" << s.m_latitude << "] "
+		<< "long[" << s.m_longitude << "] "
+		<< "cam_lat[" << s.m_cam_lat << "] "
+		<< "cam_long[" << s.m_cam_long << "] "
+		<< "velocity[" << s.m_velocity << "] "
+		<< "orientationMaxChange[" << s.m_orientation_max_change << "] "
+		<< "orientationMaxValue[" << s.m_orientation_max_value << "]"
+		<< ")";
 }
--- hashcode/hashcode/satellite.hpp
+++ hashcode/hashcode/satellite.hpp
@@ -1,27 +1,36 @@
 #pragma once
 
+#include <ostream>
 #include "photograph.hpp"
 #include "simulation.hpp"
 
+using SatelliteLine = std::array<std::string, 5>;
+
 class simulation;
 
-class satellite{
+class satellite : location {
 
 	private:
 		simulation* m_simulation;
-		double m_latitude;
-		double m_longitude;
-		double m_cam_lat;
-		double m_cam_long;
-		float m_velocity, m_orientation_max_change, m_orientation_max_value;
+
+		// orientation of the camera
+		long int m_cam_lat  = 0;
+		long int m_cam_long = 0;
+
+		int m_velocity, // arcs/turn [-500; 500]
+			m_orientation_max_change,
+			m_orientation_max_value;
+
+        friend std::ostream& operator<<(std::ostream&, const satellite&);
+
 	public:
-		satellite();
-		satellite(simulation*, double, double, float, float, float);
+		satellite(simulation*, long int, long int, int, int, int);
+        satellite(simulation*, SatelliteLine);
+
 		~satellite();
 		satellite(const satellite& satellite);
 		satellite& operator=(const satellite& satellite);
-		float getVelocity();
-		bool canTake(photograph*);
-		bool take(photograph*);
-		void moveCamera();
+
+		// void getPosition(unsigned long int time); // TODO position au temps t
+		// void moveCamera(); // TODO not sure if belongs here
 };
