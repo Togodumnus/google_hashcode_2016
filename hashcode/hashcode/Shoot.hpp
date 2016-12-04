c9d27381daaca044f5beee8ef92952fbbe8451df
Arnaud Langelier <arnaud.langelier@etu.univ-nantes.fr>
2016-12-04 11:32:04 +0100

    stub.

--- hashcode/hashcode/Shoot.hpp
+++ hashcode/hashcode/Shoot.hpp
@@ -4,20 +4,20 @@
 #include "Satellite.hpp"
 
 /**
- *    Shoot class
- **/
+*    Shoot class
+**/
 class Shoot {
 
-    protected:
-        long int m_moment  = 0;
-        Photograph* m_photo;
-        Satellite* m_sat;
+protected:
+	long int m_moment = 0;
+	Photograph* m_photo;
+	Satellite* m_sat;
 
-		friend std::ostream& operator<<(std::ostream&, const Shoot&);
+	friend std::ostream& operator<<(std::ostream&, const Shoot&);
 
-    public:
-		Shoot(long int, Photograph*, Satellite*);
-		~Shoot();
-		Shoot(const Shoot &shoot);
-		
+public:
+	Shoot(long int, Photograph*, Satellite*);
+	~Shoot();
+	Shoot(const Shoot &shoot);
+
 };
