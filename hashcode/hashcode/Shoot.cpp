ddb04925db30f7367c047885888a9a5c79fa8858
Arnaud Langelier <arnaud.langelier@etu.univ-nantes.fr>
2016-12-04 10:50:49 +0100

    finalisation class shoot

--- hashcode/hashcode/Shoot.cpp
+++ hashcode/hashcode/Shoot.cpp
@@ -1,8 +1,8 @@
 #include "Shoot.hpp"
 
-Shoot::Shoot(long int, Photograph, Satellite)
-{
-}
+Shoot::Shoot(long int moment, Photograph * photo, Satellite * sat) :
+	m_moment(moment), m_photo(photo), m_sat(sat) { }
+
 
 Shoot::~Shoot()
 {
@@ -10,9 +10,16 @@ Shoot::~Shoot()
 
 Shoot::Shoot(const Shoot & shoot)
 {
+	m_moment = shoot.m_moment;
+	m_photo = shoot.m_photo;
+	m_sat = shoot.m_sat;
 }
 
-Shoot & Shoot::operator=(const Shoot & shoot)
+std::ostream & operator<<(std::ostream & o, const Shoot & s)
 {
-	// TODO: insérer une instruction return ici
+	return o << "Shoot("
+		<< "moment[" << s.m_moment << "] "
+		<< "photo[" << s.m_photo << "] "
+		<< "sat[" << s.m_sat << "] "
+		<< ")";
 }
--- hashcode/hashcode/Shoot.hpp
+++ hashcode/hashcode/Shoot.hpp
@@ -9,16 +9,15 @@
 class Shoot {
 
     protected:
-        long int moment  = 0;
-        Photograph * photo;
-        Satellite * sat;
+        long int m_moment  = 0;
+        Photograph * m_photo;
+        Satellite * m_sat;
 
-
+		friend std::ostream& operator<<(std::ostream&, const Shoot&);
 
     public:
-		Shoot(long int, Photograph, Satellite);
+		Shoot(long int, Photograph*, Satellite*);
 		~Shoot();
 		Shoot(const Shoot &shoot);
-		Shoot& operator=(const Shoot &shoot);
-        
+		
 };
