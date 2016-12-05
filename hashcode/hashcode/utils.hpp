9490c3c4d649a084e70d08f36e36dbce3c938406
Guillaume Clochard <guillaume.clochard@etu.univ-nantes.fr>
2016-12-05 10:53:50 +0100

    Add real modulo

--- hashcode/hashcode/utils.hpp
+++ hashcode/hashcode/utils.hpp
@@ -47,3 +47,12 @@ class LogMachine {
 		}
 };
 
+
+/**
+ * real modulo
+ * @see http://stackoverflow.com/a/12089637/2058840
+ */
+inline int modulo(int a, int b) {
+	const int result = a % b;
+	return result >= 0 ? result : result + b;
+}
