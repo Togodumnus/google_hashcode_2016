c45a77c851c0437e598fb855815e47e03a3e9cc3
Guillaume Clochard <guillaume.clochard@etu.univ-nantes.fr>
2016-12-05 05:22:37 +0100

    Part 1 of BasicAlgo : building the index

--- hashcode/hashcode/BasicAlgo.cpp
+++ hashcode/hashcode/BasicAlgo.cpp
@@ -1,7 +1,33 @@
 #include "BasicAlgo.hpp"
 
+#include <vector>
 #include <iostream>
 
+#include "Collection.hpp"
+#include "utils.hpp"
+
 void BasicAlgo::solve(Simulation* s) { // TODO
-	std::cout << "TODO" << std::endl;
+
+	LogMachine log("basicAlgo");
+
+	std::vector<Collection*>& collections = s->getCollections();
+
+	log("Start building photographs index");
+
+	/**
+	 * 1.
+	 * Build latitude / longitude index on Photographs
+	 */
+
+	for(auto c = collections.begin(); c != collections.end(); c++) {
+		std::vector<Photograph*>& photographs = (*c)->getPhotographs();
+
+		for (auto p = photographs.begin(); p != photographs.end(); p++) {
+			this->latitudeIndex[(*p)->getLatitude()] = *p;
+			this->longitudeIndex[(*p)->getLongitude()] = *p;
+		}
+	}
+
+	log("End building photographs index");
+
 }
--- hashcode/hashcode/BasicAlgo.hpp
+++ hashcode/hashcode/BasicAlgo.hpp
@@ -1,9 +1,20 @@
 #pragma once
 
+#include <map>
+
 #include "Algorithm.hpp"
+#include "Location.hpp"
+#include "Simulation.hpp"
+
+class Photograph;
 
 class BasicAlgo : public Algorithm
 {
+
+	private:
+		std::map<LocationUnit, Photograph*> latitudeIndex;
+		std::map<LocationUnit, Photograph*> longitudeIndex;
+
 	public:
 		void solve(Simulation*);
 };
