efb4645521a4a7c541d92772e1f1b9c1f6366718
Cokral <thomas.coquereau@etu.univ-nantes.fr>
2016-12-14 13:34:20 +0100

    Add stuff on result

--- arbitre/arbitre/Result.cpp
+++ arbitre/arbitre/Result.cpp
@@ -1,1 +1,6 @@
 #include "Result.hpp"
+
+unsigned int Result::FigureOutScore(const char* input_file){
+
+	return 0;
+}
--- arbitre/arbitre/Result.hpp
+++ arbitre/arbitre/Result.hpp
@@ -1,11 +1,14 @@
 #pragma once
 
+#include <string>
 
 class Result {
 
 private:
-
+	unsigned int score;
 public:
 	Result();
 	~Result();
+
+	unsigned int FigureOutScore(const char* input_file);
 };
