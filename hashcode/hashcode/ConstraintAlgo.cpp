ab885439daf7df33c0d8be99642819171969a40c
Guillaume Clochard <guillaume.clochard@etu.univ-nantes.fr>
2016-12-14 19:09:05 +0100

    Create lat/lng index on photos

--- hashcode/hashcode/ConstraintAlgo.cpp
+++ hashcode/hashcode/ConstraintAlgo.cpp
@@ -1,10 +1,27 @@
 #include "ConstraintAlgo.hpp"
 
+void ConstraintAlgo::buildPhotographIndex() {
+
+	std::vector<Collection*>& collections = this->simulation->getCollections();
+
+	for(auto c = collections.begin(); c != collections.end(); c++) {
+		std::vector<Photograph*>& photographs = (*c)->getPhotographs();
+
+		for (auto p = photographs.begin(); p != photographs.end(); p++) {
+			this->photosIndex.insert(*p);
+		}
+	}
+}
+
 
 void ConstraintAlgo::solve(Simulation* s) {
 
 	this->simulation = s;
 
 	std::cout << "hello world" << std::endl;
+
+	buildPhotographIndex();
+
+	std::cout << "end" << std::endl;
 
 }
--- hashcode/hashcode/ConstraintAlgo.hpp
+++ hashcode/hashcode/ConstraintAlgo.hpp
@@ -1,13 +1,42 @@
 #pragma once
 
+#include <boost/multi_index_container.hpp>
+#include <boost/multi_index/ordered_index.hpp>
+#include <boost/multi_index/identity.hpp>
+#include <boost/multi_index/member.hpp>
+#include <boost/multi_index/mem_fun.hpp>
+
 #include "Algorithm.hpp"
 #include "Location.hpp"
 #include "Photograph.hpp"
 #include "Simulation.hpp"
 
+using namespace ::boost;
+using namespace ::boost::multi_index;
+
+using GeoPhotographIndex = multi_index_container<
+	Photograph*,
+	indexed_by<
+		ordered_non_unique<
+			const_mem_fun<Photograph, LocationUnit, &Photograph::getLatitude>
+		>,
+		ordered_non_unique<
+			const_mem_fun<Photograph, LocationUnit, &Photograph::getLongitude>
+		>
+	>
+>;
+
 class ConstraintAlgo: public Algorithm {
 
 	Simulation* simulation;
+
+	GeoPhotographIndex photosIndex;
+
+	/**
+	 * 1.
+	 * Build latitude / longitude index on Photographs
+	 */
+	void buildPhotographIndex();
 
 	public:
 		void solve(Simulation*);
