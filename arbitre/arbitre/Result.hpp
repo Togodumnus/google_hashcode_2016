664e32251d30d8d4340810ac43c4ffe2914a6768
Antoine Sauray <antoine.sauray@etu.univ-nantes.fr>
2016-12-14 17:01:11 +0100

    STUB parsing begun (again)

--- arbitre/arbitre/Result.hpp
+++ arbitre/arbitre/Result.hpp
@@ -5,12 +5,18 @@
 class Result {
 
 private:
-	unsigned int m_score;
-	long m_latitude;
-	long m_longitude;
-	long m_number_of_pictures;
-	unsigned short m_id_satellite;
-	unsigned int m_moment;
+
+	struct execution_result{
+		unsigned int m_score;
+		long m_latitude;
+		long m_longitude;
+		long m_number_of_pictures;
+		unsigned short m_id_satellite;
+		unsigned int m_moment;
+	}
+	std::vector<execution_result> m_results;
+
+	parse(std::string);
 public:
 	Result();
 	~Result();
