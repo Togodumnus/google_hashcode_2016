3d21135217b21b21c18b2a5ab9f3d46bcbae43a3
Jimmy <jimmy.leblanc@etu.univ-nantes.fr>
2016-12-14 12:27:31 +0100

    début de l'arbitre

--- arbitre/arbitre/main.cpp
+++ arbitre/arbitre/main.cpp
@@ -1,5 +1,47 @@
 #include <iostream>
+#include <ctime>
+#include <chrono>
+#include<stdio.h>
+#include<cstdlib>
+#include<iostream>
+#include<string.h>
+#include<fstream>
+#include<dirent.h>
+
+
+std::vector<string> listeFichiers;
+
+void listFile(const char* INPUT){
+        DIR *pDIR;
+        struct dirent *entry;
+        if( pDIR=opendir(INPUT) ){
+                while(entry = readdir(pDIR)){
+                        if( strcmp(entry->d_name, ".") != 0 && strcmp(entry->d_name, "..") != 0 )
+                        cout << entry->d_name << "\n";
+                }
+                closedir(pDIR);
+        }
+}
+
+
+void duration(){
+
+	std::string str(INPUT);
+    std::chrono::time_point<std::chrono::system_clock> start, end;
+    start = std::chrono::system_clock::now();
+    system("../bin/hashcode ../hashcode/hashcode/data/forever_alone.in ../hashcode/hashcode/data/forever_alone.out");
+    end = std::chrono::system_clock::now();
+ 
+    int elapsed_seconds = std::chrono::duration_cast<std::chrono::seconds>
+                             (end-start).count();
+    std::time_t end_time = std::chrono::system_clock::to_time_t(end);
+ 
+    std::cout << "finished computation at " << std::ctime(&end_time)
+              << "elapsed time: " << elapsed_seconds << "s\n";
+}
 
 int main() {
-	std::cout << "Hello world 🤓" << std::endl;
+	
+
+
 }
