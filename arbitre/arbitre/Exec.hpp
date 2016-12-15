7db3969fde2442d0287fe374f880a4fbda88cda9
Jimmy <jimmy.leblanc@etu.univ-nantes.fr>
2016-12-15 16:13:09 +0100

    cleaning

--- arbitre/arbitre/Exec.hpp
+++ arbitre/arbitre/Exec.hpp
@@ -19,13 +19,14 @@ private:
 	// name of the executable file
 	std::string m_executable;
 
-	// input files 
+	// I/O files 
 	std::vector<std::string> m_input;
 	std::vector<std::string> m_output;
-	// seconds ?	
+
+
+	// duration of the solution	
 	unsigned int m_duration;
 
-	// appeler result pour déterminer le score
 	unsigned int m_score;
 
 	// nom du fichier de jeu 
@@ -35,14 +36,10 @@ public:
 	Exec(std::string fichier);
 	~Exec();
 	
-	/*
-	 * Faut aussi que tu stockes les résultats qq part pour qu'on les envoie apres
-	 * à la classe Result qui va calculer le score et vérifier le tout
-	 */
+
 	void duration(std::string);
 	friend std::ofstream& operator<<(std::ofstream&, const Exec&);
 
-	//bool replace(std::string& str, const std::string& from, const std::string& to);
 
 	inline std::vector<std::string> getInput(){
 		return m_input;
