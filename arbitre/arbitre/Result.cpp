4037a8ded35af7f64f83c85b4ee89fbd0f06fdb5
Thomas Coquereau <thomas.coquereau@etu.univ-nantes.fr>
2016-12-16 10:40:14 +0100

    ADD tests of camera speed

--- arbitre/arbitre/Result.cpp
+++ arbitre/arbitre/Result.cpp
@@ -15,7 +15,7 @@ unsigned int Result::FigureOutScore(std::string &input_file){
 	this->parse(input_file);
 
 	// Only for the tests
-	std::string a("./arbitre/arbitre/data/forever_alone.in");
+	std::string a("./arbitre/arbitre/data/constellation.in");
 	Simulation sim(a);
 	
 	std::map<int, std::set<ResultShoot>> map_res;
@@ -24,39 +24,62 @@ unsigned int Result::FigureOutScore(std::string &input_file){
 		map_res[it.m_id_satellite].insert(ResultShoot(it.m_latitude, it.m_longitude, it.m_moment));
 	}
 
-	/*
-	 * TODO
-	 * 		Check if at time t, id_satellite's got the photo in his windows
-	 *		Check if the speed isn't too fast
-	 */
+	long lat, longi, latCam, longiCam, latCamB4, longiCamB4;
+	unsigned int timeB4;
+	int orientationValue, orientationSpeed, camSpeed=0;
 
-	long lat, longi;
-	unsigned int time;
-	int orientationValue;
-	int sum;
 	// unsigned to remove warning
 	for(unsigned int i=0; i<sim.getNumberSatellites(); i++) {
-		int j=0;
+
+		int k=0;
+		orientationSpeed = sim.getSatelliteN(i)->getOrientationMaxVelocity();
+		orientationValue = sim.getSatelliteN(i)->getOrientationMaxValue();
+
 		for(auto ind : map_res[i]){
 
 			lat = sim.getSatelliteN(i)->getLatitudeT(ind.m_time);
 			longi = sim.getSatelliteN(i)->getLongitudeT(ind.m_time);
-			orientationValue = sim.getSatelliteN(i)->getOrientationMaxValue();
 
-			if ( 	ind.getLatitude()  > ( lat + orientationValue   ||
-					ind.getLatitude()  < ( lat - orientationValue   ||
-					ind.getLongitude() > ( longi + orientationValue ||
-					ind.getLongitude() < ( longi - orientationValue ))))){
-				j++;
+			// CHECK on the satellite's shot window
+			if ( 	ind.getLatitude()  > ( lat + orientationValue   )||
+					ind.getLatitude()  < ( lat - orientationValue   )||
+					ind.getLongitude() > ( longi + orientationValue )||
+					ind.getLongitude() < ( longi - orientationValue )){
+				return 0;
 			}
 
+			// CHECK if we aren't at the first pic
+			if(k>0){
+				// Get lat cam at current t
+				latCam = abs(ind.getLatitude() - lat);
+				longiCam = abs(ind.getLongitude() - longi);
+				
+				// CHECK if lat cam speed isn't too high
+				camSpeed = abs(latCam - latCamB4) / (ind.m_time - timeB4);
+				if (camSpeed > orientationSpeed) {
+					return 0;
+				}
+
+				// CHECK if longi cam speed isn't too high
+				camSpeed = abs(longiCam - longiCamB4) / (ind.m_time - timeB4);
+				if (camSpeed > orientationSpeed) {
+					return 0;
+				}
+
+				latCamB4 = latCam;
+				longiCamB4 = longiCam;
+				timeB4 = ind.m_time; 
+			} else {
+				// If first moment we just take the pos
+				latCamB4 = abs(ind.getLatitude() - lat);
+				longiCamB4 = abs(ind.getLongitude() - longi);
+				timeB4 = ind.m_time;
+			}
+			
+			k++;
 		}
-		std::cout << j << std::endl;
-		sum+=j;
 
 	}
-	std::cout << "Number of pictures impossible : " << sum << "/" << this->m_number_of_pictures << std::endl;
-
 	return 0;
 }
 
